ebb2a09e930f3711a258fb832ce4b0bdec2fe411
nialacy23 <53440429+nialacy23@users.noreply.github.com>
2019-07-29 13:55:29 -0400

    Add files via upload

---
+++ Nia Lacy_Day 1 Code Python.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello my name is Nia")
+print("5*2")
+print(5/2)
+print(5**2)
+print((8/9)*(3))
+def multiply (a,b):
+    multiply = a*b
+    
+def sum (a,b,c):    
+    sum = (a+b+c)
+    print(sum)
+sum(6,7,8)
+
+def area (b,h):
+    area = (0.5)*b*h
+    print("The value of the base", b," multplied by the value of the height",h," multiplied by .5, equals",area)
+area(7,2)
+
